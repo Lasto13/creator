0c584f15db7459188d846c6dbcaa94ee621769b5
v-vojtek <vvojtek@enli.sk>
2015-07-26 13:01:52 +0200

    Uprava scriptov

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -13,6 +13,7 @@ function UnityProgress (dom) {
 
         var _perc = Math.round((this.progress * 100)); // / 2
         if (_perc <= 100) _loadProgress.innerHTML = _perc + ' %';
+        if (_perc === 100) setTimeout(function () { _talker.innerHTML = 'Please wait...'; }, 1000);
     }
 
     this.SetMessage = function (message) { _talker.innerHTML = message; }
@@ -20,32 +21,18 @@ function UnityProgress (dom) {
     this.Clear = function () {
         _splashSc.className = 'hide';
         setTimeout(function () {
-            document.getElementById('B0').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B1').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B2').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B3').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B4').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B5').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B8').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B9').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B13').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B12').addEventListener('click', btnClassRplc, false);
-            document.getElementById('B24').addEventListener('click', function () { console.log("Funkcia novy projekt"); }, false);
-            document.getElementById('B25').addEventListener('click', function () { console.log("Nacitat projekt"); }, false);
-            document.getElementById('B26').addEventListener('click', function () { console.log("Ulozit projekt"); }, false);
-            document.getElementById('B27').addEventListener('click', function () { console.log("Nastavenia"); }, false);
-            //Chlapi zatial nechapem zmysel tejto funkcie a podobnych je viacero
-            //document.querySelector('input:radio[name=view]').addEventListener('click', function (e) { var _id = this.value; }, false);
-            //\\
-            //document.getElementById('lot').addEventListener('click', function () { var _brw = getBrowser(); SendMessage("CameraObject", "setBrowser", browseris); }, false);
+            for (var i = 0, iL = document.querySelectorAll('a').length; i < iL; i++)
+                document.querySelectorAll('a')[i].addEventListener('click', btnClassRplc, false);
+
             document.getElementById('sp-holder').addEventListener('click', function () { this.style.display = "none"; }, false);
             document.getElementById('sp-content').addEventListener('click', function (e) { e.stopPropagation(); }, false);
 
             function btnClassRplc(a) {
                 var _pR = a.target.parentNode, _pRa = _pR.querySelectorAll('a');
-
-                for (var i = 0, iL = _pRa.length; i < iL; i++) _pRa[i].className = 'Button btn-my';
-                a.target.className = 'Button btn-my radio-view btn-my2';
+                if (_pR.id.indexOf('ButtonContainer') > -1 || _pR.id.indexOf('buttons') > -1) {
+                    for (var i = 0, iL = _pRa.length; i < iL; i++) _pRa[i].className = 'Button btn-my';
+                    a.target.className = 'Button btn-my radio-view btn-my2';
+                }
             }
         }, 1);
     }
